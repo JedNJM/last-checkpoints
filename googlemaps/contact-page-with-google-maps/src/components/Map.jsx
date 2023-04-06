8c69dc61683525e704f749c2ca675c707d567336
MedJedNJM <81960158+JedNJM@users.noreply.github.com>
2023-04-06 17:34:23 +0100

    api

---
+++ googlemaps/contact-page-with-google-maps/src/components/Map.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Map = () => {
+  return (
+    <div className="map">
+    <h2 className="map-h2">Come Visit Us At Our Campus</h2>
+
+    <div className="google-map">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={location}
+        defaultZoom={zoomLevel}
+      >
+        <LocationPin
+          lat={location.lat}
+          lng={location.lng}
+          text={location.address}
+        />
+      </GoogleMapReact>
+    </div>
+  </div>
+  )
+}
+
+export default Map
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import './map.css'
+
+const Map = ({ location, zoomLevel }) => (
+   
+  )
